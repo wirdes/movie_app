873669a11d992617f26c10cd357b2cec570c01ee
Mert Erim <mert.erim1@gmail.com>
2024-06-24 08:50:45 +0300

    chore: Update main.dart with multi-provider setup and home screen implementation

---
+++ lib/components/movie_background.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/model/movie.dart';
+
+class MovieBackground extends StatefulWidget {
+  final List<Movie> movies;
+  const MovieBackground({super.key, required this.movies});
+
+  @override
+  State<MovieBackground> createState() => _MovieBackgroundState();
+}
+
+class _MovieBackgroundState extends State<MovieBackground> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  final ScrollController scrollController = ScrollController();
+  final ScrollController scrollController2 = ScrollController();
+  final ScrollController scrollController3 = ScrollController();
+  final ScrollController scrollController4 = ScrollController();
+  List<Movie> get rightMovies => widget.movies;
+  List<Movie> get leftMovies => widget.movies.reversed.toList();
+  double top = -2000;
+  double left = -500;
+  double shake(double animation) => 5 * animation * (1 - animation);
+  @override
+  void initState() {
+    super.initState();
+    widget.movies.shuffle();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 20));
+    _controller.repeat();
+    _controller.addListener(() {
+      final value = shake(_controller.value) * 300;
+      if (scrollController.hasClients) {
+        scrollController.jumpTo(value);
+        scrollController2.jumpTo(value);
+        scrollController3.jumpTo(value);
+        scrollController4.jumpTo(value);
+      }
+
+      if (scrollController.offset >= scrollController.position.maxScrollExtent) {
+        _controller.stop();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    scrollController.dispose();
+    scrollController2.dispose();
+    scrollController3.dispose();
+    scrollController4.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+        body: GestureDetector(
+      onPanUpdate: (details) {
+        _controller.stop();
+        var topPos = top + (details.delta.dy * 1.5);
+        var leftPos = left + (details.delta.dx * 1.5);
+        setState(() {
+          top = topPos;
+          left = leftPos;
+        });
+      },
+      onPanEnd: (details) {
+        _controller.repeat();
+      },
+      child: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: [
+            AnimatedPositioned(
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeOut,
+              top: top,
+              left: left,
+              child: Row(
+                children: [
+                  MovieBackgroundWidget(size: size, controller: _controller, scrollController: scrollController, movies: rightMovies),
+                  MovieBackgroundWidget(size: size, controller: _controller, scrollController: scrollController2, movies: leftMovies, reverse: true),
+                  MovieBackgroundWidget(size: size, controller: _controller, scrollController: scrollController3, movies: rightMovies),
+                  MovieBackgroundWidget(size: size, controller: _controller, scrollController: scrollController4, movies: leftMovies, reverse: true),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
+
+class MovieBackgroundWidget extends StatelessWidget {
+  final Size size;
+  final AnimationController controller;
+  final ScrollController scrollController;
+  final List<Movie> movies;
+  final bool? reverse;
+  const MovieBackgroundWidget({
+    super.key,
+    required this.size,
+    required this.controller,
+    required this.scrollController,
+    required this.movies,
+    this.reverse = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: size.height * 5,
+      width: size.width * .8,
+      child: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return ListView(
+              reverse: reverse!,
+              controller: scrollController,
+              physics: const NeverScrollableScrollPhysics(),
+              children: movies.map((movie) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 200,
+                    height: size.height * .4,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: Colors.white, width: 0.2),
+                      image: DecorationImage(
+                        image: NetworkImage("https://image.tmdb.org/t/p/w500${movie.imageUrl}"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            );
+          }),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/components/custom_button.dart';
+import 'package:movie_app/components/movie_background.dart';
+import 'package:movie_app/model/movie.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
@@ -9,10 +14,67 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Movies(),
+        ),
+      ],
+      child: Consumer<Movies>(
+        builder: (context, authData, _) => MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Cinema',
+          theme: ThemeData(brightness: Brightness.dark),
+          routes: {
+            //  '/movie-details': (ctx) => MovieDetailsScreen(),
+            '/': (ctx) => const HomeScreen(),
+            '/home': (ctx) => const HomeScreen(),
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Scaffold(
+        body: FutureBuilder(
+          future: Provider.of<Movies>(context, listen: false).loadMovies(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (snapshot.error != null) {
+                return Center(
+                  child: Text('An error occurred! + ${snapshot.error.toString()}'),
+                );
+              } else {
+                return Consumer<Movies>(
+                  builder: (ctx, movieData, _) => SizedBox.fromSize(
+                      size: size,
+                      child: Stack(
+                        children: [
+                          MovieBackground(movies: movieData.trendingMovies),
+                          const CustomButton(),
+                        ],
+                      )),
+                );
+              }
+            }
+          },
         ),
       ),
     );
